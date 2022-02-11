b93070598c0f244ebb72f5369287e2e03e50cdff
chris05109130 <92364093+chris05109130@users.noreply.github.com>
2022-02-11 02:45:12 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Form Elements</title>
+  <link rel="stylesheet" href="main.css">
+</head>
+<body>
+  <section id="mainSection">
+    <section id="mainSection">
+        <h1 class="explain">Form Elements</h1>
+        <h1 class="explain">Make a form and put certain elements on it for people to reply to</h1>
+
+      <p class="example">Some form elements are</p>
+      <p class="example">Text Element</p>
+      <p class="explain">Text input supplies a text box for user input</p>
+      <p class="example">Name<input type="text"></p>
+      <p class="explain">Password input supplies a password box for user input</p>
+      <p class="example">PassWord<input type="password"></p>
+      <p class="explain">Submit button puts a button on the screen that users can submit data on</p>
+      <p class="example"><input type="submit" value="Submit"></p>
+      <p class="explain">CheckBox input allows users to check which box for certain options</p>
+      <input type="checkbox" name="age">10
+      <input type="checkbox" name="age">20
+      <p class="example">Radio Input allows users to check only one option out of the current options</p>
+      <input type="radio" name="numbers">1
+      <input type="radio" name="numbers">2
+      <p class="example">Textarea will provide a user with enough space to type out a long sentence on webpage</p>
+      <textarea name="name" rows="8" cols="80"></textarea>
+      <p class="example">Select input will give multiple options to select in a drop down list</p>
+      <select>
+        <option value="#">Hello</option>
+        <option value="#">There</option>
+        <option value="#">How</option>
+      </select>
+
+  </section>
+
+</body>
+</html>
